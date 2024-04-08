d8a105d098c49e83726e581e1b5e31bc6925f0e7
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2024-04-08 16:38:06 +0100

    removed one static review

--- src/app/all-reviews/page.tsx
+++ src/app/all-reviews/page.tsx
@@ -216,10 +216,6 @@ export default function Sample() {
             rating={4.0}
             review="There is no stable electricity. The roads are fairly good and there is a sense of community. The drainage system is poor and most residents litter their surroundings. There are several grocery stores and Supermarkets."
           />
-          <Review
-            rating={4.0}
-            review="There is no stable electricity. The roads are fairly good and there is a sense of community. The drainage system is poor and most residents litter their surroundings. There are several grocery stores and Supermarkets."
-          />
 
           {reviews.map((review, index) =>(
             <Review key={index} rating={review.rating} review={review.reviewText} />
